b5fea3216416b1cb885bb7679960205dc01e3c50
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-09-14 17:45:26 +0300

    Create models for posts and hashtags

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    hashtags = models.ManyToManyField('Hashtag', blank=True)
+
+    def __str__(self):
+        return self.title[:50]
+
+
+class Hashtag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
