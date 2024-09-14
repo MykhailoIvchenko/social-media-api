1608fdfac71ae7c20b40274384a35380ba2c7978
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-09-14 17:06:01 +0300

    Add url for logout

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserProfileViewSet, UserFollowViewSet
+from .views import UserProfileViewSet, UserFollowViewSet, logout
 
 router = DefaultRouter()
 router.register(r'profile', UserProfileViewSet, basename='user-profile')
 router.register(r'follow', UserFollowViewSet, basename='user-follow')
 
 urlpatterns = [
+    path('logout/', logout, name='logout'),
     path('', include(router.urls)),
 ]
