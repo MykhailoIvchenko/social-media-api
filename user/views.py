9bf804a4d6d871d224acae7d109a3aa5c78c7f6b
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-09-14 16:18:21 +0300

    Add view for the user profile

--- user/views.py
+++ user/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import User
+from .permissions import IsOwnerOrReadOnly
+from .serializers import UserProfileSerializer
 
-# Create your views here.
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        queryset = User.objects.all()
+        email = self.request.query_params.get('email', None)
+        if email:
+            queryset = queryset.filter(email__icontains=email)
+        return queryset
+
