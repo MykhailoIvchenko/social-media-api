8a9e024d126a23bba36e4419e810604f807d69e7
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-09-14 18:01:57 +0300

    Add serializers for posts

---
+++ posts/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Post, Hashtag
+
+
+class HashtagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hashtag
+        fields = ["name"]
+
+
+class PostImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ("id", "image")
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ["id", "user", "title"]
+
+
+class PostDetailsSerializer(PostSerializer):
+    hashtags = HashtagSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ["id", "user", "title", "content", "image", "created_at", "updated_at", "hashtags"]
